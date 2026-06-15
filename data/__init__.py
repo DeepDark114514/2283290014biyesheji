ffa197f5b61890d3dac135d7bdb5c083e12ae2fd
于陈远 <202283290014@nuist.edu.cn>
2026-06-16 01:35:46 +0800

    chore: remove unused data/ and configs/ directories

--- data/__init__.py
+++
@@ -1,4 +0,0 @@
-from datasets.mfqev2_dataset import MFQEv2Dataset, build_dataloader
-from datasets.inference_utils import tile_predict, pad_frame
-
-__all__ = ['MFQEv2Dataset', 'build_dataloader', 'tile_predict', 'pad_frame']
--- data/raw/README.md
+++
@@ -1,1 +0,0 @@
-Please refer to this [repository](https://github.com/RyanXingQL/MFQEv2.0/wiki/MFQEv2-Dataset).
